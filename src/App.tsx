9d861c951a43376439c33a0f881d40e3692cf5d8
Damien Osmani <osmani.damien@gmail.com>
2023-02-28 21:24:54 +0100

    update better practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,44 @@
 
+import { Button } from '@mui/material';
 import React from 'react';
 
 import Progress from './progress'
 
 
-interface AppState{
- list:any[],
- index:number
+interface AppState {
+  list: any[],
+  index: number
 }
 
-interface AppProps{
+interface AppProps {
 
 }
 const baseUrl = `https://progressBar`;
 
 
-class App extends React.Component<AppProps,AppState>{
-  constructor(props) {
-    super(props);
-    this.state = {
-      list:[],
-      index:1
+class App extends React.Component<AppProps, AppState>{
+  state = {
+    list: [],
+    index: 1,
+    dev: true
   };
-    this.handle=this.handle.bind(this)
-    this.addChild=this.addChild.bind(this)
-    this.handleDestroy=this.handleDestroy.bind(this)
-  }
 
-  handleDestroy(id){
-    let t=this.state.list.filter(p=>p.id!==id)
-    this.setState({list:t})
+  handleDestroy = (id) => {
+    console.log(this.state.list)
+    let t = this.state.list.filter(p => p.id !== id)
+    console.log(t)
+    this.setState({ list: t })
   }
-  addChild(dura){
-    this.setState({list:this.state.list.concat({dur:dura,id:this.state.index}),index:this.state.index+1})
-  }
+  addChild = (dura) => {
+    this.setState({ list: this.state.list.concat({ dur: dura, id: this.state.index }), index: this.state.index + 1 })
 
-  handle(data:any){
-    if(data?.data?.action==="show"){
+  }
+  devchild = (e) => {
+    this.addChild(5000)
+    return e
+  }
+  handle = (data: any) => {
+    if (data?.data?.action === "show") {
       this.addChild(data.data.duration)
     }
   }
@@ -44,20 +46,21 @@ class App extends React.Component<AppProps,AppState>{
     window.addEventListener('message', this.handle);
   }
   componentWillUnmount(): void {
-    window.removeEventListener('message',this.handle)
+    window.removeEventListener('message', this.handle)
   }
-  render(){
-      return (
-        <React.Fragment>
-             {<div style={{bottom:'10%',left:'33%',display:'block',justifyContent:'center',width:'34%',position:'absolute'}} >          
-          {this.state.list.map((items)=>{
-            if(items){
+  render() {
+    return (
+      <>
+        {this.state.dev && <Button variant='outlined' onClick={this.devchild} > Dev</Button>}
+        {<div style={{ bottom: '10%', left: '33%', display: 'block', justifyContent: 'center', width: '34%', position: 'absolute' }} >
+          {this.state.list.map((items) => {
+            if (items) {
               return <Progress key={items.id} id={items.id} dura={items.dur} callback={this.handleDestroy}></Progress>
             }
           })}
         </div>}
-      </React.Fragment>
-      )
+      </>
+    )
   }
 
 }
--- src/progress.tsx
+++ src/progress.tsx
@@ -3,69 +3,64 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import LinearProgress from '@mui/material/LinearProgress';
 
-interface AppState{
-progress:number,
-isToggleOn:string,
-duration:number
+interface AppState {
+  progress: number,
+  isToggleOn: string
 }
 
-interface AppProps{
-    dura:number,
-    id:number,
-    callback:any
+interface AppProps {
+  dura: number,
+  id: number,
+  callback: any,
+  key: any
 }
 
-class Progress extends React.Component<AppProps,AppState>{
-  constructor(props) {
-    super(props);
-    this.state = {
-      isToggleOn: 'visible',
-      progress: 0,
-      duration:props.dura
+class Progress extends React.Component<AppProps, AppState>{
+  state = {
+    isToggleOn: 'visible',
+    progress: 0,
   };
-    this.launch=this.launch.bind(this)
-  }
 
-  launch(duration){
-    const interval=duration/20
+
+  launch = (duration) => {
+    const interval = duration / 20
     setTimeout(() => {
-      const timer = setInterval(()=>{
+      const timer = setInterval(() => {
 
-        if(this.state.progress===100){
+        if (this.state.progress === 100) {
           setTimeout(() => {
 
             clearInterval(timer)
-            this.setState({progress:0,isToggleOn:'hidden'})
+            this.setState({ progress: 0, isToggleOn: 'hidden' })
             this.props.callback(this.props.id)
           }, 250);
-  
+
         }
-        else{
-          this.setState({progress:this.state.progress+5})
+        else {
+          this.setState({ progress: this.state.progress + 5 })
         }
-        },interval)
+      }, interval)
     }, 150);
   }
 
-  componentDidMount():void{
-      console.log('this.state.duration', this.state.duration)
-      this.launch(this.state.duration)
+  componentDidMount(): void {
+    this.launch(this.props.dura)
   }
 
 
-  render(){
-      return (
-        <React.Fragment>
-          <Box sx={{visibility:this.state.isToggleOn,width:'100%',marginTop:'4px'}}> 
-        <div  style={{position:'relative',backgroundColor:"#30373f",borderRadius:'10px',borderLeftWidth:'1px',borderRightWidth:'1px',outline:'currentcolor non 0px',margin:'auto',padding:'18px'}}>
-        <Box sx={{width:'100%'}}>
-        <LinearProgress sx={{height:'16px'}} variant="determinate" value={this.state.progress} />        
-      </Box>
-      </div>
-      </Box>
-      </React.Fragment>
-      
-      )
+  render() {
+    return (
+      <>
+        <Box sx={{ visibility: this.state.isToggleOn, width: '100%', marginTop: '4px' }}>
+          <div style={{ position: 'relative', backgroundColor: "#30373f", borderRadius: '10px', borderLeftWidth: '1px', borderRightWidth: '1px', outline: 'currentcolor non 0px', margin: 'auto', padding: '18px' }}>
+            <Box sx={{ width: '100%' }}>
+              <LinearProgress sx={{ height: '16px' }} variant="determinate" value={this.state.progress} />
+            </Box>
+          </div>
+        </Box>
+      </>
+
+    )
   }
 
 }
